d30d31e19670279f4878092e09805e53fa0a61e4
Estevão <estevao.polli@hotmail.com>
2026-04-26 23:02:52 -0300

    Adicionando e traduzindo comentários

--- VideoTranscritor.py
+++ VideoTranscritor.py
@@ -10,43 +10,45 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 class Application:
     def __init__(self, mainWindow):
-        #Caixa de texto para a transcrição
+        #Textbox for transcription / Caixa de texto para a transcrição
         self.textBox = tk.Text(mainWindow, width=100, height = 30)
         self.textBox.pack(pady=10)
 
-        #Caixa de texto para o nome do arquivo
+        #Textbox for view the file path / Caixa de texto para visualizar o caminho do arquivo
         self.dir = tk.Entry(mainWindow, width=100)
         self.dir.pack(pady=10)
         self.dir.config(state="readonly")
 
-        #Botão para selecionar o arquivo
+        #Button for select file / Botão para selecionar o arquivo
         self.selectFileBtn = tk.Button(mainWindow, text="Selecionar Arquivo", command=self.get_file)
         self.selectFileBtn.pack(pady=10)
 
-        #Botão para gerar a transcrição
+        #Button for generate the transcription / Botão para gerar a transcrição
         self.transcriptionBtn = tk.Button(mainWindow, text="Transcrever", command=self.get_transcription)
         self.transcriptionBtn.pack(pady=10)
 
-        #Botão para iniciar a criação do video com legenda
+        #Button for create the new video with captions / Botão para iniciar a criação do novo video com a legenda
         self.startBtn = tk.Button(mainWindow, text="Gerar Video", command=self.create_video)
         self.startBtn.pack(pady=10)
 
-    #Pega o arquivo
+    #Get the file / Pega o arquivo
     def get_file(self):
         fileSelect = tk.filedialog.askopenfile(initialdir="/Videos")
         self.clip = VideoFileClip(fileSelect.name)
         self.fileName = fileSelect.name
+
+        #Edit the file path textBox / Edita a caixa de texto do caminho do arquivo
         self.dir.config(state="normal")
         self.dir.delete(0, tk.END)
         self.dir.insert(0, self.fileName)
         self.dir.config(state="readonly")
 
-    #Pega a transcrição
+    #Get the transcription / Pega a transcrição
     def get_transcription(self):
         if os.path.exists(self.fileName):
             print("Arquivo existe")
 
-            #Modelo de IA para transcrever
+            #Transcription AI Model / Modelo de IA de transcrição
             model = whisper.load_model("turbo", device="cuda")
             result = model.transcribe(self.fileName, language="pt", fp16=False)
             print(result)
@@ -54,6 +56,7 @@ class Application:
             self.textBox.delete("1.0", tk.END)
             self.edited_subscriptions = []
 
+            #Transform the Whisper's output into a readable SRT pattern / Transforma a saída do Whisper para o um padrão SRT legível
             for sentence in result["segments"]:
                 total_ms_start = int(round(sentence['start'] * 1000))
                 total_ms_end = int(round(sentence['end'] * 1000))
@@ -78,18 +81,18 @@ class Application:
                     "transcription": sentence['text']
                 })
                 self.textBox.insert(tk.END,f"{sentence['text']}\n")
-            print(self.edited_subscriptions)
         else:
             print("Arquivo nao existe")
+
 
     def create_video(self):
         print(self.edited_subscriptions)
 
-        #Adiciona a edição feita no item "transcription"
+        #Add the edit made to the "transcription item" / Adiciona a edição feita no item "transcription"
         for i in range(len(self.edited_subscriptions)):
             self.edited_subscriptions[i].update({"transcription": self.textBox.get(f"{i + 1}.1", f"{i + 1}.end")})
 
-        #Cria o arquivo srt
+        #Create the srt file / Cria o arquivo srt
 
         with open("captions.srt", "w", encoding="UTF-8") as file:
             for caption in self.edited_subscriptions:
@@ -97,6 +100,7 @@ class Application:
                 file.write(f"{caption['start']} -> {caption['end']}\n")
                 file.write(f"{caption['transcription']}\n \n")
 
+        #Text settings / Configurações do texto
         generator = lambda text: TextClip(
             text = text,
             font="arial.ttf",
@@ -106,12 +110,14 @@ class Application:
             size=(int(self.clip.w * 0.9), None))
 
         print(generator)
+
+        #Composite and Render the video / #Compõe e renderiza o vídeo
         sub = SubtitlesClip("captions.srt", make_textclip = generator, encoding="utf-8")
         video = CompositeVideoClip([self.clip, sub.with_position(("center", 0.85), relative=True)])
         video.write_videofile("result.mp4", fps=self.clip.fps, codec="libx264", audio_codec="aac")
 
 
-#Tela principal
+#Main Screen / Tela principal
 mainWindow = tk.Tk()
 mainWindow.geometry("1080x720")
 mainWindow.title("Legendador de Vídeo")
